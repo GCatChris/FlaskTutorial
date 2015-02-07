2cc19e19b90c43ed8dd276aea2bc89a03d2cda9e
GCatChris <gcatchris@gmail.com>
2015-02-07 09:20:51 -0800

    Update
    2.7.15

--- app/views.py
+++ app/views.py
@@ -5,6 +5,17 @@ from app import app
 @app.route('/index')
 def index():
 	user = {'nickname': 'Chris'} #Fake user
+	posts = [ # fake array of posts
+		{
+			'author': {'nickname': 'Fred'},
+			'body': 'Beautiful Day in my living room!'
+		},
+		{
+			'author': {'nickname': 'Doug'},
+			'body': 'The new Max Martin song kicks ass!'
+		}
+		]
 	return render_template('index.html',
 				title='Home',
-				user=user)
+				user=user,
+				posts=posts)
